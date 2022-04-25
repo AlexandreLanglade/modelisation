28e352533b5fcb43297b002bde282afa6bdf9920
AlexandreLanglade <alexandre.langlade@outlook.fr>
2022-04-25 15:41:47 +0200

    Step 3 : tables

---
+++ modelisation/tables.py
@@ -0,0 +1,31 @@
+import django_tables2 as tables
+
+from netbox.tables import NetBoxTable, ChoiceFieldColumn
+from .models import Topology, Member
+
+class TopologyTable(NetBoxTable):
+
+    name = tables.Column(
+        linkify=True
+    )
+
+    member_count = tables.Column()
+
+    class Meta(NetBoxTable.Meta):
+        model = Topology
+        fields = ('pk', 'id', 'name', 'slug', 'member_count', 'description', 'actions')
+        default_columns = ('name', 'slug', 'member_count', 'description')
+
+
+class MemberTable(NetBoxTable):
+
+    topology = tables.Column(
+        linkify=True
+    )
+
+    type = ChoiceFieldColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = Member
+        fields = ('pk', 'id', 'name', 'slug', 'type', 'topology', 'description', 'actions')
+        fields = ('name', 'slug', 'type', 'topology', 'description', 'actions')
