2e95d809e5769a5b4236ec197ae9bc124c4a6157
AlexandreLanglade <alexandre.langlade@outlook.fr>
2022-04-28 17:08:08 +0200

    Step 7 : navigations

--- modelisation/navigation.py
+++ modelisation/navigation.py
@@ -2,9 +2,10 @@ from extras.plugins import PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
 
-member_buttons = [
+
+fabricpath_butons = [
     PluginMenuButton(
-        link='plugins:modelisation:member_add',
+        link='plugins:modelisation:fabricpath_add',
         title='Add',
         icon_class='mdi mdi-plus-thick',
         color=ButtonColorChoices.GREEN
@@ -20,16 +21,29 @@ topology_butons = [
     )
 ]
 
+member_buttons = [
+    PluginMenuButton(
+        link='plugins:modelisation:member_add',
+        title='Add',
+        icon_class='mdi mdi-plus-thick',
+        color=ButtonColorChoices.GREEN
+    )
+]
+
 menu_items = (
     PluginMenuItem(
-        link='plugins:modelisation:member_list',
-        link_text='Members',
-        buttons=member_buttons
+        link='plugins:modelisation:fabricpath_list',
+        link_text='Fabric Paths',
+        buttons=fabricpath_butons
     ),
     PluginMenuItem(
         link='plugins:modelisation:topology_list',
         link_text='Topologies',
         buttons=topology_butons
-
+    ),
+    PluginMenuItem(
+        link='plugins:modelisation:member_list',
+        link_text='Members',
+        buttons=member_buttons
     ),
 )
