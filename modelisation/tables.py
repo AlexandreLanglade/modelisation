4c9ae0c59c76caf24f8ed108228f2a72e53e3e24
AlexandreLanglade <alexandre.langlade@outlook.fr>
2022-04-28 15:28:02 +0200

    Step 3 : tables

--- modelisation/tables.py
+++ modelisation/tables.py
@@ -1,7 +1,20 @@
 import django_tables2 as tables
 
 from netbox.tables import NetBoxTable, ChoiceFieldColumn
-from .models import Topology, Member
+from .models import FabricPath, Topology, Member
+
+class FabricPathTable(NetBoxTable):
+
+    name = tables.Column(
+        linkify=True
+    )
+
+    topology_count = tables.Column()
+
+    class Meta(NetBoxTable.Meta):
+        model = FabricPath
+        fields = ('pk', 'id', 'name', 'slug', 'topology_count', 'description', 'actions')
+        default_columns = ('name', 'slug', 'topology_count', 'description')
 
 class TopologyTable(NetBoxTable):
 
@@ -9,11 +22,15 @@ class TopologyTable(NetBoxTable):
         linkify=True
     )
 
+    fabric_path = tables.Column(
+        linkify=True
+    )
+
     member_count = tables.Column()
 
     class Meta(NetBoxTable.Meta):
         model = Topology
-        fields = ('pk', 'id', 'name', 'slug', 'member_count', 'description', 'actions')
+        fields = ('pk', 'id', 'name', 'slug', 'fabric_path', 'member_count', 'description', 'actions')
         default_columns = ('name', 'slug', 'member_count', 'description')
 
 
